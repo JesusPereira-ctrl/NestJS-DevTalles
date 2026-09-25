9774571bf800820fd9242d0261a75d89c6409625
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-09-24 23:23:30 -0300

    feat: objects and interfaces

--- 01-TypeScript-Intro/src/main.ts
+++ 01-TypeScript-Intro/src/main.ts
@@ -1,10 +1,11 @@
 import './style.css';
 
-import { age, name } from './bases/01-types';
+// import { age, name } from './bases/01-types';
+import { bulbasaur } from './bases/02-objects';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = `
-  <h1>Hello ${name} ${age}!!!</h1>
+  <h1>Hello ${bulbasaur.name}!!!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
