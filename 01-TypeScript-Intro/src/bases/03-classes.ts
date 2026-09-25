e03f24683d994ffeb4f05366594f7346e6ac2f23
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-09-24 23:49:49 -0300

    feat: classes and abbreviated form

---
+++ 01-TypeScript-Intro/src/bases/03-classes.ts
@@ -0,0 +1,11 @@
+export class Pokemon {
+  constructor(
+    public readonly id: number,
+    public name: string,
+  ) {}
+}
+
+export const charmander = new Pokemon(4, 'Charmander');
+
+// charmander.id = 10;
+// charmander.name = 'Mew';
--- 01-TypeScript-Intro/src/main.ts
+++ 01-TypeScript-Intro/src/main.ts
@@ -2,10 +2,11 @@ import './style.css';
 
 // import { age, name } from './bases/01-types';
 // import { pokemons } from './bases/02-objects';
+import { charmander } from './bases/03-classes';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = `
-  <h1>Hello Vite!!!</h1>
+  <h1>Hello ${charmander.name} ${charmander.id}!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
