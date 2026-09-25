27676ed38d1d92457e5f5bf79a0d19eac4a58766
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-09-24 23:28:19 -0300

    feat: types in arrays

--- 01-TypeScript-Intro/src/main.ts
+++ 01-TypeScript-Intro/src/main.ts
@@ -1,11 +1,11 @@
 import './style.css';
 
 // import { age, name } from './bases/01-types';
-import { bulbasaur } from './bases/02-objects';
+// import { pokemons } from './bases/02-objects';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = `
-  <h1>Hello ${bulbasaur.name}!!!</h1>
+  <h1>Hello Vite!!!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
