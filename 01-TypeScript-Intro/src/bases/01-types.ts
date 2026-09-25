3e3b097c29fed8d2986f0105e9671698359f7075
JesusPereira-ctrl <jesuspereirap100@gmail.com>
2026-09-24 23:12:14 -0300

    feat: types and bases about modules

---
+++ 01-TypeScript-Intro/src/bases/01-types.ts
@@ -0,0 +1,20 @@
+export let name: string = 'Fernando';
+export const age: number = 35;
+export const isValid: boolean = true;
+
+name = 'Melissa';
+// name = 123;
+// name = true;
+
+export const templateString = ` Esto es un string
+multilinea
+que puede tener
+" dobles
+' simples
+inyectar valores ${name}
+expresiones ${1 + 1}
+números: ${age}
+booleanos: ${isValid}
+`;
+
+console.log(templateString);
--- 01-TypeScript-Intro/src/main.ts
+++ 01-TypeScript-Intro/src/main.ts
@@ -1,8 +1,10 @@
 import './style.css';
 
+import { age, name } from './bases/01-types';
+
 const app = document.querySelector<HTMLDivElement>('#app')!;
 
 app.innerHTML = `
-  <h1>Hello Vite!!!</h1>
+  <h1>Hello ${name} ${age}!!!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `;
